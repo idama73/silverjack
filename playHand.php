5288c98ce22c112d3aab401e8c2fc28fee97a7f5
Cean Manzano <cemanzano@csumb.edu>
2018-09-19 22:34:36 +0000

    displayCards and game logic

---
+++ playHand.php
@@ -0,0 +1,47 @@
+<?php
+function playHand(&$deck) {
+    $total = 0;
+    $suit = array("clubs", "hearts", "diamonds", "spades");
+    echo "<hr>";
+    echo "<div class='playerHand'>";
+    while ($total < 42) {
+        if ($total > 38) break;
+        $card = array_pop($deck);
+        $suitName = $suit[floor($card / 13)];
+        $cardValue = $card % 13 + 1;
+        echo "<img class='card' src='deck/$suitName/$cardValue.png' alt='$cardValue $suitName' >";
+        $total += $cardValue;
+    }
+    echo "<span class='individualScore'> score: $total</span>";
+    echo "</div>";
+}
+
+function playGame() {
+    $deck = range(0, 51);
+    shuffle($deck);
+
+    $playerScores = array();
+    for ($i = 0; $i < 4; $i++) {
+        $playerScores[] = playHand($deck);
+    }
+    echo "<hr>";
+    return $playerScores;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <style>
+        .playerHand {
+            margin: auto;
+            padding: auto;
+            padding-left: 30%;
+        }
+        .card {
+            margin: 10px;
+        }
+    </style>
+    <body>
+        <?php playGame(); ?>
+    </body>
+</html>
